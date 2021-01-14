94d196eb6e080dcd6f194c213b779aa87d1ac89a
AMBIKA198 <74423294+AMBIKA198@users.noreply.github.com>
2021-01-14 14:06:51 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+	
+ 
+ $serverName="localhost";
+ $userName="root";
+ $password="";
+ $dbName="Spark_bank";
+
+ $con=mysqli_connect($serverName,$userName,$password,$dbName);
+
+?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+<style>
+body
+{
+  background-image:url("img/back2.jpg");
+  background-size:100%;
+}
+h1
+{
+  margin-left:250px;
+  margin-right:250px;
+  box-shadow:1px 2px 10px 5px white;
+  color:white;
+}
+h2:hover
+{
+  color:white;
+  margin-top:30px;
+}
+
+nav
+{
+  box-shadow:1px 2px 10px 5px white;
+}
+</style>
+       <div class="container-fluid">
+       <nav class="navbar navbar-expand-md navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Digital Banking</a>    
+    </div>
+  
+    <ul class="nav navbar-nav ">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">User <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="createuser.php">Create User</a></li>
+          <li><a href="transfermoney.php">transfer Money</a></li>
+          <li><a href="transaction.php">Trasaction History</a></li>
+        </ul>
+      </li>
+      
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+                
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            
+                <div class="heading text-center my-5">
+                  <h1 style="font-size:3.5em">Basic Banking System</h1>
+                  <h2>Spark Foundation Project</h2>
+
+               </div>
+      </div>
+      
+      
+  </body>
+  <footer style="margin-top:30%">
+     <center><p style="color:#fff;">Designed by Ambika</p></center>
+</footer>
+</html>
